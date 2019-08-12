9de18e03d386523968204125a9408743b6ed90c8
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-08-12 13:34:57 +0200

    Utilisation de interface  et manque les requettes

--- body/interface_bdd.py
+++ body/interface_bdd.py
@@ -4,10 +4,52 @@ from models import User, Store, Category, Product, History, ProductCategory, Pro
 
 
 class BddQueries:
-    p_categories = []    
     
-    def proposed_categories(self):
-        pass
+    def Choice_1(self):
+        while True:
+            while True:
+                categories_index_list =[]
+                p_categories = Category.select().execute()
+                print("I propose you", len(p_categories), "categories!")
+                for index, value in enumerate(p_categories):
+                    print (index, value )
+                    categories_index_list.append(index)
+                print("Select one category please!", categories_index_list)
+                try:
+                    cat_choice =int(input(" Which category do you want to chose?"))
+                    if cat_choice in categories_index_list:
+                        c_category = p_categories[cat_choice]
+                        print( "You have chosen", c_category, "!")
+                    else:
+                        continue 
+                    pass
+                except ValueError:
+                    continue
+                product_categorie_index_list = []
+                query_products_categorie = ProductCategory.select()
+                    
+                for index, value in enumerate(query_products_categorie):
+                    print(index, value)
+                    product_categorie_index_list.append(index)
+                print("Select one product please please!\n", product_categorie_index_list)
+                try:
+                    product_choice =int(input(" Which category do you want to chose?"))
+                    pass
+                except ValueError:
+                    pass
+                break
 
-if if __name__ == "__main__":
-    pass
+    def choice_2(self):
+        while True:
+            query_history = History.select()
+            history_index_list =[]
+            p_categories = Category.select().execute()
+            print("You have", len(query_history), "recorded products!")
+            for index, value in enumerate(query_history):
+                print (index, value )
+                categories_index_list.append(index)
+            break
+
+if __name__ == "__main__":
+    BddQueries.Choice_1(BddQueries)
+    BddQueries.choice_2(BddQueries)
--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 import sys
 sys.path.append(r'C:\Users\loupy\OneDrive\Bureau\OPENCLASSROOMS\Projet5\PurBeurre\data')
 sys.path.append(r'C:\Users\loupy\OneDrive\Bureau\OPENCLASSROOMS\Projet5\PurBeurre\body')
-"""from interface_bdd import BddQueries"""
+from interface_bdd import BddQueries
 from data.updateBDD import InsertOrDeleteData
-from data.models import History, Product, User, Category, ProductCategory
+from data.models import User
 from pprint import pprint
 """
 Main page of the program.
@@ -19,33 +19,12 @@ print("Select one of those three choises please!!")
 
 while True:
     main_choice = input(" 1 - Which food do you want to replace?\n 2 - Find my substituted foods.\n 3 - Update the food-database!\n 4 - To quit!\n What is your choice")
-    if main_choice == '1': 
-        while True:
-            while True:
-                categories_index_list =[]
-                p_categories = Category.select().execute()
-                print("I propose you", len(p_categories), "categories!")
-                for index, value in enumerate(p_categories):
-                    print (index, value )
-                    categories_index_list.append(index)
-                print("Select one category please!", categories_index_list)
-                try:
-                    cat_choice =int(input(" Which food do you want to replace?"))
-                    c_category = p_categories[cat_choice]
-                    print( "You have chosen", c_category, "!") 
-                    pass
-                except ValueError:
-                    continue
-                product_categorie_index_list = []
-                """query_products_categorie = (ProductCategory._id).select(ProductCategory._id).where(ProductCategory.categories == c_category).limit(20)"""
-                for index, value in enumerate(query_products_categorie):
-                    print(index, value)
-                    product_categorie_index_list.append(index)
-                print("Select one product please please!\n", product_categorie_index_list)
-                break
+    if main_choice == '1':
+        BddQueries.Choice_1(BddQueries) 
             
     if main_choice == '2':
-        pass
+        BddQueries.choice_2(BddQueries)
+
     if main_choice == "3":
         InsertOrDeleteData.update_all_products()
         pass
