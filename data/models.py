b02b414af4129b57e9a59eaa22bd693569320fda
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-08-23 22:42:40 +0200

    changement pour mysql

--- data/models.py
+++ data/models.py
@@ -1,39 +1,39 @@
 from peewee import *
 
-pg_db = PostgresqlDatabase('Pure_Beurre', user='PureBeurre', password='12345678',
-                           host='localhost', port=5432) # Connect to data base.
-
 
 """
-Defines and create tables.
+Defines and create database.
 """
+mysql_db = MySQLDatabase('Pur_beurre')
 
-class User(Model):
+class BaseModel(Model):
+    """A base model that will use our MySQL database"""
+    class Meta:
+        database = mysql_db
+
+class User(BaseModel):
     """ Class to define the User table."""
     id = PrimaryKeyField()
     u_name = CharField(unique=True)
 
     class Meta:
-        database = pg_db
         db_table = 'user'
 
-class Store(Model):
+class Store(BaseModel):
     """ Class to define the Store table."""
     stores_tags = CharField(primary_key=True)
 
     class Meta:                
-        database = pg_db
         db_table = 'store'
 
-class Category(Model):
+class Category(BaseModel):
     """ Class to define the Category table."""    
     categories = CharField(primary_key=True)
 
     class Meta:                
-        database = pg_db
         db_table = 'category'
 
-class Product(Model):
+class Product(BaseModel):
     """ Class to define the Product table."""
     _id = CharField(primary_key=True)
     ingredients_text_fr = TextField()
@@ -42,10 +42,9 @@ class Product(Model):
     url = TextField()
 
     class Meta:                
-        database = pg_db
         db_table = 'product'
 
-class History(Model):
+class History(BaseModel):
     """ Class to define the History table."""
     id = PrimaryKeyField()
     _id = ForeignKeyField(User, backref='history')
@@ -53,27 +52,24 @@ class History(Model):
     remplacement_product = ForeignKeyField(Product, related_name = 'remplacement_product', backref='history')
 
     class Meta:          
-        database = pg_db
         db_table = 'history'
 
-class ProductCategory(Model):
+class ProductCategory(BaseModel):
     """ Class to define the Product category table."""
     _id = ForeignKeyField (Product, backref='product_category')
     categories = ForeignKeyField (Category, backref='product_category')
 
     class Meta:
         primary_key = CompositeKey('_id', 'categories')                
-        database = pg_db
         db_table = 'product_category'
 
-class ProductStore(Model):
+class ProductStore(BaseModel):
     """ Class to define the Product Store table."""
     _id = ForeignKeyField(Product, backref='product_store')
     stores_tags = ForeignKeyField(Store, backref='product-store')
 
     class Meta:
         primary_key = CompositeKey('_id', 'stores_tags')                
-        database = pg_db
         db_table = 'product_store'
 
 if __name__ == "__main__":
