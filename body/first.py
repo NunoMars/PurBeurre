32671a1275c9354b789687d63e3a368ac4ff7fbd
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-09-05 11:24:03 +0200

    changement question

--- body/first.py
+++ body/first.py
@@ -11,11 +11,11 @@ class MainMenu:
 
         while True:
             menu_choice = input(
-                user_name+" choisissez parmis ces choix ;-)!\n\
+                user_name+"  choisissez parmis ces choix ;-)!\n\
             1-Quel aliment voulez-vous remplacer?\n\
             2-Trouver mes aliments substitués\n\
-            3-Inserer les produits dans la base de données, \
-                premiére utilisation\n\
+            3-Inserer les produits dans la\
+                 base de données,premiére utilisation\n\
             4-Quiter\n\
             [1], [2], [3] ou [4] ?")
 
