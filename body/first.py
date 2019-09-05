f553ca4236ed3815d4a8e64f87eee7f42e2823ab
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-09-05 11:29:46 +0200

    correction

--- body/first.py
+++ body/first.py
@@ -14,7 +14,7 @@ class MainMenu:
                 user_name+"  choisissez parmis ces choix ;-)!\n\
             1-Quel aliment voulez-vous remplacer?\n\
             2-Trouver mes aliments substitués\n\
-            3-Inserer les produits dans la base de données,premiére utilisation\n\
+            3-Inserer les produits dans la base de données (premiére utilisation)\n\
             4-Quiter\n\
             [1], [2], [3] ou [4] ?")
 
