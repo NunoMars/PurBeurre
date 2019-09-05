08737c5acb1297db2b28220d64e52fd70742b96a
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-09-05 14:19:20 +0200

    pep8

--- datas/models.py
+++ datas/models.py
@@ -6,10 +6,12 @@ Defines and create database.
 """
 mysql_db = MySQLDatabase('Pur_beurre')
 
+
 class BaseModel(Model):
     """A base model that will use our MySQL database"""
     class Meta:
         database = mysql_db
+
 
 class User(BaseModel):
     """ Class to define the User table."""
@@ -19,6 +21,7 @@ class User(BaseModel):
     class Meta:
         db_table = 'user'
 
+
 class Store(BaseModel):
     """ Class to define the Store table."""
     stores_tags = CharField(primary_key=True)
@@ -26,12 +29,14 @@ class Store(BaseModel):
     class Meta:                
         db_table = 'store'
 
-class Category(BaseModel):
-    """ Class to define the Category table."""    
-    categories = CharField(primary_key=True)
 
-    class Meta:                
+class Category(BaseModel):
+    """ Class to define the Category table."""
+    categories = CharField(primary_key=True)
+    
+    class Meta:    
         db_table = 'category'
+
 
 class Product(BaseModel):
     """ Class to define the Product table."""
@@ -44,23 +49,28 @@ class Product(BaseModel):
     class Meta:                
         db_table = 'product'
 
+
 class History(BaseModel):
     """ Class to define the History table."""
     id = ForeignKeyField(User, backref='history')
-    chosen_product = ForeignKeyField(Product, related_name = 'chosen_product ', backref='history')
-    remplacement_product = ForeignKeyField(Product, related_name = 'remplacement_product', backref='history')
+    chosen_product = ForeignKeyField(
+        Product, related_name='chosen_product ', backref='history')
+    remplacement_product = ForeignKeyField(
+        Product, related_name='remplacement_product', backref='history')
 
     class Meta:         
         db_table = 'history'
 
+
 class ProductCategory(BaseModel):
     """ Class to define the Product category table."""
-    _id = ForeignKeyField (Product, backref='product_category')
-    categories = ForeignKeyField (Category, backref='product_category')
+    _id = ForeignKeyField(Product, backref='product_category')
+    categories = ForeignKeyField(Category, backref='product_category')
 
     class Meta:
         primary_key = CompositeKey('_id', 'categories')                
         db_table = 'product_category'
+
 
 class ProductStore(BaseModel):
     """ Class to define the Product Store table."""
@@ -78,4 +88,4 @@ if __name__ == "__main__":
     Product.create_table()
     History.create_table()
     ProductCategory.create_table()
-    ProductStore.create_table() 
+    ProductStore.create_table()
