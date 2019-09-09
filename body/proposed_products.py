6a1db08707afd651262fb33ccee441994e145eff
Nuno Marcelino <nuno.ricardo.mars@gmail.com>
2019-09-09 15:48:51 +0200

    changement phrase d'action

--- body/proposed_products.py
+++ body/proposed_products.py
@@ -53,9 +53,8 @@ class ProposedProducts:
         while True:
             web_page_ask2 = input(
                 "Voulez-vous voir leur page internet?\n" +
-                "[O] = Oui  [N] = Non" +
-                "(Possibilité de choisir un autre produit" +
-                " dans la categorie)")
+                "[O] = Oui  [N] = Non (Retour au menu)"
+            )
             if web_page_ask2 == "O" or web_page_ask2 == "o":
                 webbrowser.open_new(c_product.url)
                 webbrowser.open_new(proposed_product.url)
